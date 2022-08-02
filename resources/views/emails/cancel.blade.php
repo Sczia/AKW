4e4cb8dddfe975028c7bcaab410aba4609c5e4f9
Krysia~ <krysialee023@gmail.com>
2022-08-02 20:06:54 +0800

    09-02-2022

--- resources/views/emails/cancel.blade.php
+++ resources/views/emails/cancel.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <h2 class="text-danger">APPOINTMENT CANCELED</h2>
-<p class=" text-center"> Appointment Keeper World</p>
+<p class="text-center"> Appointment Keeper World</p>
 
 <p> Sorry, your Appointment Request has been canceled. Your chosen date is not available. Kindly book another appointment. </p>
 
